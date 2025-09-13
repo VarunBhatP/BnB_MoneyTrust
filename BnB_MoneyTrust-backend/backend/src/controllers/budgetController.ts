2928c531eac57e2fe356e6b4ad2a89304afb50f2
pavan-ak1 <kustagipavan30@gmail.com>
2025-09-13 12:24:54 +0530

    refactored codebase

---
+++ BnB_MoneyTrust-backend/backend/src/controllers/budgetController.ts
@@ -0,0 +1,12 @@
+import {Request, Response} from 'express';
+import { prisma } from '';
+import {StatusCodes} from 'http-status-codes';
+
+export const createBudget = async (req:Request, res:Response)=>{
+    try{
+
+    }catch(error){
+        console.log(error);
+        res.status()
+    }
+}
