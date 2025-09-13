b8e7070316dd9b61c6b1a373a36f740d5071e1ee
pavan-ak1 <kustagipavan30@gmail.com>
2025-09-14 02:30:42 +0530

    DEleted next frontend

--- frontend/src/@types/global.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// TypeScript path alias support
-declare module '@/components/layout/*';
-declare module '@/components/sections/*';
